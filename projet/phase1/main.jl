78d75b41027f47415c92b3f2f585f3be552c50db
Monssaf Toukal <tmonssaf@gmail.com>
2020-10-01 13:53:14 -0400

    kruskal now works for graph given in lab

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -14,6 +14,8 @@ include(joinpath(@__DIR__, "connected_component.jl"))
 lab_nodes = [Node("a",1),Node("b",2),Node("c",3),Node("d",4),Node("e",5),Node("f",6),Node("g",7),Node("h",8),Node("i",9)]
 lab_edges = [Edge(("a","b"),4),Edge(("a","h"),8),Edge(("b","c"),8),Edge(("b","h"),11),Edge(("c","d"),7),Edge(("c","f"),4),Edge(("c","i"),2),Edge(("d","e"),9),Edge(("d","f"),14),Edge(("e","f"),10),Edge(("f","g"),2),Edge(("g","h"),1),Edge(("g","i"),6),Edge(("h","i"),7)]
 graph = Graph("laboratory graph", lab_nodes, lab_edges)
-show(graph)
 
-show(kruskal(graph))
+# show(graph)
+mst = kruskal(graph)
+show(mst, graph)
+plot_graph(mst)
